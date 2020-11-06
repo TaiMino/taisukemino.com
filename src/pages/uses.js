71b01f80d47835ed1189a39d4c0788988a297996
Tai <4382091+taisukemino@users.noreply.github.com>
2020-11-07 00:39:56 +0900

    add a uses page

---
+++ src/pages/uses.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react'
+
+class Uses extends Component {
+  render() {
+    return (
+      <div>
+      <h1
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '90%'
+        }}      
+      >
+        Uses
+        </h1>
+        <ul
+          style={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '80%'
+          }}
+        >
+          <li>
+            I <strong>manage important tasks</strong> using{' '}
+            <a href="https://www.notion.so/">Notion</a>{' '}
+          </li>
+          <li>
+            <p>
+              I <strong>report my daily and weekly tasks</strong> using{' '}
+              <a href="https://discordapp.com/">Discord</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>manage my plans</strong> using{' '}
+              <a href="https://calendar.google.com/calendar/">
+                Google Calendar
+              </a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>schedule meetings</strong> using{' '}
+              <a href="https://calendly.com/">
+                Calendly
+              </a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>manage my miscellaneous tasks</strong> using{' '}
+              <a href="https://trello.com">Trello</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>manage my emails</strong> using{' '}
+              <a href="https://superhuman.com/">
+                Superhuman
+              </a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>write code</strong> using{' '}
+              <a href="https://code.visualstudio.com/">VSCode</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>manage code versions</strong> using{' '}
+              <a href="https://git-scm.com">Git</a> and{' '}
+              <a href="http://github.com">GitHub</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>design</strong> using{' '}
+              <a href="https://www.invisionapp.com/studio">Invision Studio</a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>get design inspiration</strong> using{' '}
+              <a href="https://pinterest.com">Pinterest</a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>understand, organize and improve ideas</strong> using{' '}
+              <a href="https://bear.app">Bear</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>write</strong> using{' '}
+              <a href="https://ia.net/writer">iA Writer</a>,{' '}
+              <a href="https://www.grammarly.com/">Grammarly</a> and{' '}
+              <a href="http://www.hemingwayapp.com">Hemingway Editor</a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>post on Twitter</strong> using{' '}
+              <a href="https://buffer.com/">Buffer</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>memorize vocabulary</strong> using{' '}
+              <a href="https://apps.ankiweb.net/">Anki</a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>pin good websites</strong> using{' '}
+              <a href="https://pinboard.in">Pinboard</a>
+            </p>
+          </li>
+          <li>
+            I <strong>save good articles</strong> using{' '}
+            <a href="https://raindrop.io">Raindrop</a>
+          </li>
+          <li>
+            I <strong>save great websites and articles</strong> using{' '}
+            <a href="https://www.instapaper.com/">Instapaper</a>
+          </li>
+          <li>
+            I <strong>subscribe to interesting blogs</strong> using{' '}
+            <a href="http://feedly.com">Feedly</a>{' '}
+          </li>
+          <li>
+            I <strong>save interesting books</strong> using{' '}
+            <a href="http://goodreads.com">Goodreads</a>
+          </li>
+          <li>
+            I <strong>recall what I've read</strong> using{' '}
+            <a href="https://readwise.io">Readwise</a>
+          </li>
+          <li>
+            <p>
+              I <strong>save interesting movies</strong> using{' '}
+              <a href="http://imdb.com">IMDb</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>manage my passwords</strong> using{' '}
+              <a href="https://1password.com/">1Password</a>{' '}
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>change my key bindings</strong> using{' '}
+              <a href="https://www.keyboardmaestro.com/">Keyboard Maestro</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I{' '}
+              <strong>
+                find&amp;open files, search the web and do other small things
+              </strong>{' '}
+              using <a href="https://www.alfredapp.com">Alfred</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              I <strong>plan and track my calisthenic training</strong> using{' '}
+              <a href="https://www.strong.app/">Strong</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Uses
